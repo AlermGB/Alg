8356e7027087754c09a2073b053188b7a8004a7e
AlermGB <116220194+AlermGB@users.noreply.github.com>
2023-07-13 22:23:28 +0300

    Update Main.java

--- 3/Main.java
+++ 3/Main.java
@@ -1,18 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        LinkedListS listS = new LinkedListS();
+
+        listS.add(1);
+        listS.add(2);
+        listS.add(3);
+        listS.add(4);
+        listS.add(7);
+        listS.print();
+        System.out.println();
+        listS.reverse();
+        listS.print();
+
+        System.out.println();
         LinkedList list = new LinkedList();
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(7);
-
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(17);
         list.print();
         System.out.println();
         list.reverse();
-
         list.print();
     }
 }
